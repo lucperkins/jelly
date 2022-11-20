874ad22fc6c7d7ff19ba83f72a524ff5c947dd81
Luc Perkins <lucperkins@gmail.com>
2022-11-20 19:12:57 -0300

    Add id and path parameters

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use gray_matter::engine::YAML;
 use gray_matter::{Matter, Pod};
 use serde::{Deserialize, Serialize};
+use std::collections::hash_map::DefaultHasher;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 
 #[derive(Serialize)]
 struct Page {
-    //id: String,
-    //path: String,
+    id: String,
+    path: String,
     title: String,
     body: String,
 }
@@ -50,18 +51,29 @@ impl Page {
             None => FrontMatter::default(),
         };
 
-        let title: String = front.title.unwrap_or(String::from(path.to_string_lossy()));
+        let path_str = &String::from(path.to_string_lossy());
+
+        let title: String = front.title.unwrap_or(String::from(path_str));
+
+        let id = base64::encode(&title);
 
         Ok(Page {
-            body: result.content,
+            id,
+            path: String::from(path_str),
             title,
+            body: result.content,
         })
     }
 }
 
 fn main() {
-    let path = "./example/index.md";
-    let file = File::open(path).unwrap();
+    let path = "example/index.md";
     let page = Page::from_path(&Path::new(path)).unwrap();
-    println!("(title: {}, body: {})", page.title, page.body);
+    println!(
+        "(id: {id}, path: {path}, title: {title}, body: {body})",
+        id = page.id,
+        path = page.path,
+        title = page.title,
+        body = page.body
+    );
 }
