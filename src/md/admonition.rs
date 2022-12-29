d6c2635cf85a8ab1c9563c297c17ca2f46644f85
Luc Perkins <lucperkins@gmail.com>
2022-12-29 14:51:23 -0300

    Begin with implementation

--- src/md/admonition.rs
+++ src/md/admonition.rs
@@ -1,5 +1,9 @@
 use markdown_it::{
-    parser::block::{BlockRule, BlockState},
+    parser::{
+        block::{BlockRule, BlockState},
+        inline::InlineRoot,
+    },
+    plugins::cmark::block::fence::CodeFence,
     MarkdownIt, Node, NodeValue, Renderer,
 };
 
@@ -9,6 +13,19 @@ enum AdmonitionKind {
     Info,
     Success,
     Warning,
+}
+
+impl ToString for AdmonitionKind {
+    fn to_string(&self) -> String {
+        use AdmonitionKind::*;
+
+        String::from(match *self {
+            Danger => "danger",
+            Info => "info",
+            Success => "success",
+            Warning => "warning",
+        })
+    }
 }
 
 impl Default for AdmonitionKind {
@@ -23,17 +40,64 @@ struct Admonition {
 }
 
 impl NodeValue for Admonition {
-    fn render(&self, node: &Node, fmt: &mut dyn Renderer) {}
+    fn render(&self, node: &Node, fmt: &mut dyn Renderer) {
+        let div_attrs = &[("admonition-type", self.kind.to_string())];
+
+        fmt.cr();
+        fmt.open("div", div_attrs);
+        fmt.contents(&node.children);
+        fmt.close("div");
+        fmt.cr();
+    }
 }
 
 struct AdmonitionScanner;
 
 impl BlockRule for AdmonitionScanner {
     fn run(state: &mut BlockState) -> Option<(Node, usize)> {
-        None
+        let line = state.get_line(state.line);
+
+        if line.starts_with("::") {
+            Some((
+                Node::new(Admonition {
+                    kind: AdmonitionKind::Info,
+                }),
+                1,
+            ))
+        } else {
+            None
+        }
     }
 }
 
 pub fn add_admonition_rule(md: &mut MarkdownIt) {
     md.block.add_rule::<AdmonitionScanner>();
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    use crate::md::{ast, render};
+
+    #[test]
+    fn render_admonitions() {
+        let cases: Vec<(&str, &str)> = vec![(
+            indoc! {"
+            ::alert
+            This doesn't work.
+
+            ::alert
+            But this does.
+            ::
+        "},
+            indoc! {"<p>::alert\nThis doesn't work.</p>\n"},
+        )];
+
+        for (input, expected_html) in cases {
+            let tree = ast(input);
+            let html = render(&tree);
+            assert_eq!(expected_html, &html);
+        }
+    }
 }
