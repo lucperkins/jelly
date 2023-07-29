2476406be700d638726f3bb4027397458f322497
Luc Perkins <lucperkins@gmail.com>
2023-07-29 16:36:55 -0700

    Update Nix setup, including CI

--- flake.nix
+++ flake.nix
@@ -1,72 +1,71 @@
 {
-  description = "A Nix-flake-based Rust development environment";
+  description = "Jelly";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
     { self
-    , flake-utils
     , nixpkgs
     , rust-overlay
     }:
 
     let
-      cargoSha256 = "sha256-DY0V7c2Dn/Ium5vdMOga5NXQOFohHDCqIF+AQ9FsPHY=";
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = self.overlays; };
+      });
 
       meta = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
       inherit (meta) name version;
-
+    in
+    {
       overlays = [
-        (import rust-overlay)
+        rust-overlay.overlays.default
         (self: super: {
           rustToolchain = super.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         })
       ];
-    in
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system overlays; };
-      inherit (pkgs) mkShell writeScriptBin;
 
-      xFunc = cmd: writeScriptBin "x-${cmd}" ''
-        cargo watch -x ${cmd}
-      '';
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default =
+          let
+            xFunc = cmd: pkgs.writeScriptBin "x-${cmd}" ''
+              cargo watch -x ${cmd}
+            '';
 
-      ci = writeScriptBin "ci" ''
-        cargo fmt --check
-        cargo clippy
-        cargo build --release
-        cargo test
-      '';
+            ci = pkgs.writeScriptBin "ci" ''
+              cargo fmt --check
+              cargo clippy
+              cargo build --release
+              cargo test
+            '';
 
-      scripts = [
-        ci
-        (builtins.map (cmd: xFunc cmd) [ "build" "check" "run" "test" ])
-      ];
-    in
-    {
-      packages.default = pkgs.rustPlatform.buildRustPackage {
-        pname = name;
-        inherit version;
-        src = ./.;
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+            scripts = [
+              ci
+              (builtins.map (cmd: xFunc cmd) [ "build" "check" "run" "test" ])
+            ];
+          in
+          pkgs.mkShell {
+            packages = with pkgs; [
+              rustToolchain
+              cargo-edit
+              cargo-watch
+            ] ++ scripts;
+          };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.rustPlatform.buildRustPackage {
+          pname = name;
+          inherit version;
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
         };
-      };
-
-      devShells.default = mkShell {
-        buildInputs = (with pkgs; [
-          rustToolchain
-          cargo-edit
-          cargo-watch
-          rust-analyzer
-          cachix
-          direnv
-        ]) ++ scripts;
-      };
-    });
+      });
+    };
 }
