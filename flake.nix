31c0910968cd013c3741e56ed4177fe611e1f13b
Luc Perkins <lucperkins@gmail.com>
2023-08-19 16:51:01 -0700

    Move Nix overlays out of self

--- flake.nix
+++ flake.nix
@@ -13,21 +13,21 @@
     }:
 
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; overlays = self.overlays; };
-      });
-
-      meta = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
-      inherit (meta) name version;
-    in
-    {
       overlays = [
         rust-overlay.overlays.default
         (self: super: {
           rustToolchain = super.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         })
       ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+
+      meta = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
+      inherit (meta) name version;
+    in
+    {
 
       devShells = forEachSupportedSystem ({ pkgs }: {
         default =
