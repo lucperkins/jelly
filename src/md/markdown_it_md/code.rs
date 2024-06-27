66c584b95e99cab6e8b0316b68033da781820ebe
Luc Perkins <lucperkins@gmail.com>
2024-06-26 20:55:32 -0700

    Add code block metadata for showLineNumbers

--- src/md/markdown_it_md/code.rs
+++ src/md/markdown_it_md/code.rs
@@ -6,22 +6,50 @@ use markdown_it::{
 
 use super::highlight::Highlighter;
 
+#[derive(Debug, Default)]
+struct Metadata {
+    language: Option<String>,
+    show_line_numbers: bool,
+}
+
+impl Metadata {
+    fn parse(s: &str) -> Self {
+        let mut metadata = Metadata {
+            language: None,
+            show_line_numbers: false,
+        };
+        let mut parts = s.split_whitespace();
+
+        if let Some(part) = parts.next() {
+            metadata.language = Some(String::from(part));
+        }
+
+        while let Some(part) = parts.next() {
+            if part == "showLineNumbers" {
+                metadata.show_line_numbers = true;
+            }
+        }
+        metadata
+    }
+}
+
 #[derive(Debug)]
 struct FancyCodeBlock {
-    meta: Option<String>,
+    meta: Metadata,
     content: String,
 }
 
 impl NodeValue for FancyCodeBlock {
     fn render(&self, _: &Node, fmt: &mut dyn Renderer) {
+        // TODO: make default language configurable
         let default_lang = String::from("text");
-        let lang = self.meta.as_ref().unwrap_or(&default_lang);
-        let language = format!("language-{}", lang);
-        let pre_attrs = vec![("class", language)];
+        let lang = self.meta.language.as_ref().unwrap_or(&default_lang);
+
+        let pre_attrs = vec![("class", format!("language-{}", lang))];
 
         let higlighter = Highlighter::default();
 
-        let code = match higlighter.highlight(lang, &self.content) {
+        let code = match higlighter.highlight(&lang, &self.content) {
             Ok(html) => html,
             Err(e) => e.to_string(),
         };
@@ -41,20 +69,22 @@ struct FancyCodeBlockRule;
 impl CoreRule for FancyCodeBlockRule {
     fn run(root: &mut Node, _: &MarkdownIt) {
         root.walk_post_mut(|node, _| {
-            let mut meta: Option<&String> = None;
+            let mut meta: Option<Metadata> = None;
             let mut content: Option<&String> = None;
 
             if let Some(code) = node.cast::<CodeBlock>() {
                 content = Some(&code.content);
             }
             if let Some(code) = node.cast::<CodeFence>() {
-                meta = Some(&code.info);
+                meta = Some(Metadata::parse(&code.info));
                 content = Some(&code.content);
             }
 
             if let Some(content) = content {
+                println!("{meta:?}");
+
                 node.replace(FancyCodeBlock {
-                    meta: meta.map(String::from),
+                    meta: meta.unwrap_or_default(),
                     content: String::from(content),
                 })
             }
